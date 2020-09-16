8092c47b4fb3ba7e92d9a130299209982179bbca
ryan clayson <ryanclayson@hotmail.com>
2020-09-16 11:53:08 -0400

    implemented CartesianPoint in main

--- OOP3200-F2020-Lesson2/main.cpp
+++ OOP3200-F2020-Lesson2/main.cpp
@@ -72,14 +72,19 @@ private:
 int main()
 {
 	// Declarations
+	
 	// a point called 'origin', initialized to 0, 0
+	CartesianPoint origin = CartesianPoint(0, 0);
+	
 	// a point called 'destination'
+	CartesianPoint destination;
+
 	int inputX; // temporary input variable for x
 	int inputY; // temporary input variable for y
+	
 	// to store the distance between two points
-
-	double myDouble = ConsoleInput::ReadDouble();
-
+	double distance;
+	
 	try
 	{
 		// Input
@@ -88,22 +93,26 @@ int main()
 		cout << "\nEnter coordinates of the destination point: " << endl;
 		// prompt for, read, and store x coordinate:
 		cout << "X: ";
-
+		inputX = ConsoleInput::ReadInteger();
+		destination.SetX(inputX);
 
 
 		// prompt for, read, and store y coordinate:
 		cout << "Y: ";
-
+		inputY = ConsoleInput::ReadInteger();
+		destination.SetY(inputY);
 
 
 		// Processing
 		// determine the distance between the two points
-
+		distance = origin.GetDistanceTo(destination);
 
 		// Output 
 		// Show the points and the distance
 		cout << fixed << setprecision(3); // formatting
-
+		cout << "\nThe distance between " << origin.ToString()
+			<< " and " << destination.ToString() << " is "
+			<< distance << "." << endl;
 
 	}
 
